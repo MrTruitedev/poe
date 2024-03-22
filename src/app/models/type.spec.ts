76ec0f561aaf870f4a2f3d0eabd1ea566c7e1d5c
Thomas <thomas.turti@outlook.fr>
2024-03-22 15:56:44 +0100

    delete app component

--- src/app/models/type.spec.ts
+++ src/app/models/type.spec.ts
@@ -4,4 +4,14 @@ describe('Type', () => {
   it('should create an instance', () => {
     expect(new Type()).toBeTruthy();
   });
+
+  it('Should give an empty string for lastname attribute', () =>{
+    const objet = new Type(); // Instance type de la classe Type
+    expect(objet.lastname).toEqual('');
+  })
+
+  it('Should give a Date instance for birthdate', () => {
+    const objet = new Type();
+    expect(objet.birthDate).toBeInstanceOf(Date);
+  })
 });
--- src/app/models/type.ts
+++ src/app/models/type.ts
@@ -3,4 +3,24 @@ export class Type {
      * @var {string} lastname of any person
      */
     public lastname: string = '';
+    
+    /**
+     * @var {number} age of the captain
+     */
+    public age: number = 0;
+
+    /**
+     * @var {boolean} isMale or not
+     */
+    public isMale: boolean = false;
+
+    public skills: Array<string> = [];
+
+    public madSkills: string[] = [];
+
+    public birthDate: Date = new Date();
 }
+
+
+
+
